3b9e77fc0025ab735682672c9641f7303c72f4f4
AIPI25JUN02 <sarveshc30@gmail.com>
2026-02-16 17:42:42 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -147,6 +147,8 @@ async def book_appointment():
 if __name__ == "__main__":
     try:
         print("Starting Flask server with scheduled daily reminders at 1:27 PM...")
-        app.run(debug=True, host="0.0.0.0", port=5000)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(debug=False, host="0.0.0.0", port=port)
+
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
